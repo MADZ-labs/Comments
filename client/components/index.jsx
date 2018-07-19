c1863766ea6f99fe07e06165832a563b4e769b56
Michael Chan <michaelchan@Michaels-MacBook-Pro.local>
2018-07-19 13:57:36 -0700

    reconfigure webpack

---
+++ client/components/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>hello</div>
+    )
+  }
+};
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
