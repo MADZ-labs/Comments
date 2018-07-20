aa4cdf7dcf2671050e3562b997b79563f5e90666
Michael Chan <michaelchan@Michaels-MacBook-Pro.local>
2018-07-19 18:06:31 -0700

    add more sample html

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,6 @@ module.exports = {
     "extends": "airbnb",
     rules: {
         'no-console': 'off',
-    },
-    {
-      "globals": {
-        "document": true,
-        "foo": true,
-        "window": true
-      }
     }
 };
 
