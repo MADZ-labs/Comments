cf380ca3baa9c3768e0362f5db90878f40ca15fe
Michael Chan <mikey.chan1@gmail.com>
2018-07-22 17:29:24 -0700

    Implement random data generation functions

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
+const faker = require('faker');
+
 const commentSchema = mongoose.Schema({
+  avatar: String,
   username: {
     type: String,
   },
@@ -11,3 +14,42 @@ const commentSchema = mongoose.Schema({
 });
 
 let Comment = mongoose.model('Comment', commentSchema);
+
+
+let fakeComments = [];
+
+let generateComments = () => {
+  for (let i = 0; i < 21; i += 1) {
+    let comment = {
+      avatar: faker.internet.avatar(),
+      username: faker.internet.userName() ,
+      backer: randomBackerTier(),
+      comment: faker.lorem.sentences(),
+      date: ,
+
+    }
+    fakeComments.push(comment);
+  }
+}
+
+let insertComments = (arr) => {
+  Comment.insertMany(arr, (err) => {
+    if(err) {
+      // log.info('failed to store the data');
+    } else {
+      // log.info('stored');
+    }
+  })
+
+}
+
+let randomBackerTier = () => {
+  let tiers = ['Backer', 'Superbacker'];
+  let random = Math.floor(Math.random * Math.floor(tiers.length))
+  return tiers[random];
+}
+// userName
+
+module.exports = {
+  generateComments
+}
