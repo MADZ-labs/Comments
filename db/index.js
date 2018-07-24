a8b55f45ddd363622ad18b39c97c6010c55368a1
Michael Chan <mikey.chan1@gmail.com>
2018-07-23 23:51:23 -0700

    change schema to reflect service plan better

--- db/index.js
+++ db/index.js
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 const mongoose = require('mongoose');
 
 const Log = require('log');
@@ -6,7 +8,6 @@ const log = new Log('info');
 
 mongoose.connect('mongodb://localhost/test');
 
-const faker = require('faker');
 
 const commentSchema = mongoose.Schema({
   avatar: String,
@@ -14,25 +15,37 @@ const commentSchema = mongoose.Schema({
   backer: String,
   comment: String,
   date: Number,
+  project: {
+    projectName: String,
+    projectID: Number,
+  },
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
 
-const randomBackerTier = () => {
-  const tiers = ['Backer', 'Superbacker'];
+const tiers = ['Backer', 'Superbacker'];
+const projects = [['Indie Movie', 100], ['Crypto Currency Project', 101], ['Hipster Notebook', 102]];
+
+const randomizer = (arr) => {
   const random = Math.floor(Math.random * Math.floor(tiers.length))
-  return tiers[random];
+  return arr[random];
 };
+
 
 const generateComments = () => {
   const fakeComments = [];
+  const randProj = randomizer(projects);
   for (let i = 0; i < 21; i += 1) {
     const comment = {
       avatar: faker.internet.avatar(),
       username: faker.internet.userName(),
-      backer: randomBackerTier(),
+      backer: randomizer(tiers),
       comment: faker.lorem.sentences(),
       date: faker.date.recent(),
+      project: {
+        projectName: randProj[0],
+        projectID: randProj[1],
+      },
     };
     fakeComments.push(comment);
   }
