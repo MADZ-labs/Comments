4bfcf7554b1bd800f38b3b782d1c411076fd804c
Michael Chan <mikey.chan1@gmail.com>
2018-07-25 13:16:28 -0700

    implement GET route for comments

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const Log = require('log');
+const db = require('../db/index');
 
 const log = new Log('info');
 
@@ -10,6 +11,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../client/dist/')));
+
+app.get('/comments', (req, res) => {
+  db.generateComments((err, data) => {
+    if (err) {
+      log.info(err);
+    } else {
+      log.info(data);
+    }
+  });
+});
 
 const port = 3003;
 if (process.env.NODE_ENV !== 'test') {
