c1225a72f24bc1cd45c9e009e19e39911e161b2a
Michael Chan <mikey.chan1@gmail.com>
2018-07-25 13:57:54 -0700

    fix syntax error on sending status

--- server/tests/default.test.js
+++ server/tests/default.test.js
@@ -4,7 +4,7 @@ const app = require('../server.js');
 describe('Test /', () => {
   it('should return status code of 200', async () => {
     const response = await request(app).get('/');
-    expect(response.statusCode).toBe(200);
+    expect(response.status).toBe(200);
   });
   it('should return an object of data', async () => {
     const response = await request(app).get('/');
