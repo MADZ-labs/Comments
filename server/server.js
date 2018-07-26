adf0d717e66d86111849d28e9713cf4b141e8ace
Michael Chan <mikey.chan1@gmail.com>
2018-07-25 22:58:04 -0700

    code clean up and coverage changes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const Log = require('log');
+const db = require('../db/index');
+
+const log = new Log('info');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../client/dist/')));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/comments', (req, res) => {
+  db.generateComments((err, randComments) => {
+    if (err) {
+      log.info(err);
+    } else {
+      res.json(randComments);
+      res.end();
+    }
+  });
+});
 
 const port = 3003;
 if (process.env.NODE_ENV !== 'test') {
