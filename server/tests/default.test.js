0dd9609baceec90a84fcfa18d11a90a6bb318a7a
Michael Chan <mikey.chan1@gmail.com>
2018-07-25 19:25:34 -0700

    add done function to async jest test

--- server/tests/default.test.js
+++ server/tests/default.test.js
@@ -2,8 +2,9 @@ const request = require('supertest');
 const app = require('../server.js');
 
 describe('Test /', () => {
-  it('should return status code of 200', async () => {
+  it('should return status code of 200', async (done) => {
     const response = await request(app).get('/comments');
     expect(response.statusCode).toBe(200);
+    done();
   });
 });
