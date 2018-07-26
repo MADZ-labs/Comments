346b066d6d142df3c510bbd4e3278bb8412a7f72
Michael Chan <mikey.chan1@gmail.com>
2018-07-25 22:57:48 -0700

    add done function to async test

--- server/tests/default.test.js
+++ server/tests/default.test.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
 const app = require('../server.js');
 
+
 describe('Test /', () => {
-  it('should return status code of 200', async () => {
+  it('should return status code of 200', async (done) => {
     const response = await request(app).get('/comments');
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toEqual(200);
+    done();
   });
 });
