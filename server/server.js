90081b03f7231c7d8c5814a176f89fdba38b366d
Michael Chan <mikey.chan1@gmail.com>
2018-07-26 00:02:11 -0700

    add simple get for testing

--- server/server.js
+++ server/server.js
@@ -1,33 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const Log = require('log');
-const db = require('../db/index');
-
-const log = new Log('info');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../client/dist/')));
-
-// app.get('/', (req, res) => res.send('Hello World!'));
-
-app.get('/comments', (req, res) => {
-  db.generateComments((err, randComments) => {
-    if (err) {
-      log.info(err);
-    } else {
-      res.json(randComments);
-      res.end();
-    }
-  });
-});
+const app = require('./app');
 
 const port = 3003;
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`listening on ${port}`));
 }
-
-module.exports = app;
