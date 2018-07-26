bc9cfb9f8da5d6a7817134257ba9f1ab476a7cdc
Michael Chan <mikey.chan1@gmail.com>
2018-07-26 14:46:11 -0700

    move axios function to proper component

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import CommentEntry from './commentEntry';
 
 export default class Comments extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      comments: [],
     };
   }
 
+  getComments() {
+    axios.get('/comments')
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          comments: response,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
+    const { comments } = this.state;
     return (
       <div>
         <h1>
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
 import Comments from './Comments';
 
 import '../stylesheets/main.scss';
@@ -9,36 +8,16 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      comments: [],
+
     };
   }
 
-  getComments() {
-    axios.get('/comments')
-      .then((response) => {
-        console.log(response);
-        this.setState({
-          comments: response,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
   render() {
-    const { comments } = this.state;
     return (
       <div>
         <h1>Comment Section</h1>
         <div>
-          <Comments comments={comments} />
-        </div>
-        <div>
-          goodbye
-        </div>
-        <div>
-          react
+          <Comments />
         </div>
       </div>
     );
