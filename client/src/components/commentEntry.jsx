d33210065ae78c6408790660e4c8cdfb6bfd4b1a
Michael Chan <mikey.chan1@gmail.com>
2018-07-27 15:49:25 -0700

    Implement commentEntry with styled components

--- client/src/components/commentEntry.jsx
+++ client/src/components/commentEntry.jsx
@@ -3,35 +3,77 @@ import moment from 'moment';
 import styled from 'styled-components';
 
 const Comment = styled.div`
-  max-width: 1040px;
-  margin-right: auto;
-  margin-left: auto;
+  padding-top: 1.8rem !important;
+  padding-bottom: 1.8rem !important;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-bottom: 1.8rem;
 `;
 
-const Div = styled.div`
-  font-size: 12px;
+const MainComment = styled.div`
+  padding-top: 5px;
+  padding-left: 1.8rem;
+  margin-left: 1.8rem;
+`;
+
+const Author = styled.span`
+  font-weight: bold;
+`;
+
+const Backer = styled.span`
+  padding: 5px 7px;
+  background-color: #037362;
+  border-radius: 0;
+  color: #FFFFFF;
+  margin-left: 5px;
+  font-size: 12px !important;
+  line-height: 1.1em;
 `;
 
 const Image = styled.img`
-  height: 25px;
+  height: 30px;
   width: auto;
+  float: left;
+  border-radius: 50%;
 `;
 
-const Text = styled.div`
-  font-size: 10px;
+const Date = styled.span`
+  font-size: 12px;
+  margin-bottom: 10px;
+  color: #3D3D66;
+  top: 12px;
+  right: 10px;
+  margin-left: 10px;
+`;
+
+const Text = styled.p`
+  word-wrap: break-word;
+  margin-bottom: 5px;
+  color: #020621;
+  font-size: 14px;
 `;
 
 const CommentEntry = ({ comment }) => (
   <Comment>
-    <Div>
+    <div>
       <Image src={comment.avatar} alt="" />
-      {comment.username}
-      {comment.backer} 
-      {moment(comment.date).startOf('hour').fromNow()}
-    </Div>
-    <Text>
-      {comment.comment}
-    </Text>
+    </div>
+    <MainComment>
+      <div>
+        <Author>
+          {comment.username}
+        </Author>
+        <Backer>
+          {comment.backer}
+        </Backer>
+        <Date>
+          {moment(comment.date).startOf('hour').fromNow()}
+        </Date>
+      </div>
+      <Text>
+        {comment.comment}
+      </Text>
+    </MainComment>
   </Comment>
 );
 
