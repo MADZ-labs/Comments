905ed9be299da8224ac12a15476c3ccf70210d45
Michael Chan <mikey.chan1@gmail.com>
2018-07-27 17:50:28 -0700

    add sidepanel and post elements

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -14,9 +14,24 @@ const InnerSec = styled.div`
 const AllComments = styled.div`
   width: 66.6666667%;
   float: left;
-  padding-left: 1.8rem !important;
-  padding-right: 1.8rem !important;
-  padding-bottom: 1.8rem !important;
+  padding: 0px 1.8rem, 1.8rem;
+  box-sizing: border-box;
+`;
+
+const FAQ = styled.div`
+  width: 33.333333%;
+  float: right;
+  box-sizing: border-box;
+  border-left: 0.3rem solid #DCDEDD;
+  padding: 1.2rem 0rem 1.2rem 1.8rem;
+`;
+
+const CommentBox = styled.div`
+  margin-right: 20px;
+  margin-bottom: 4.2rem;
+  background: #F0F0F0;
+  text-align: center;
+  padding: 20px;
 `;
 
 export default class Comments extends Component {
@@ -47,7 +62,24 @@ export default class Comments extends Component {
     return (
       <Section>
         <InnerSec className="inner-sec">
+          <FAQ>
+            Use this space to cheer the creator along, and talk to your fellow backers.
+            <div>
+              <br/>
+              Have a question?
+            </div>
+            <div>
+              <a href="http://">
+              Check out the FAQ
+              </a>
+            </div>
+          </FAQ>
           <AllComments className="comment-section">
+            <CommentBox>
+              <div>
+                Only backers can post comments.
+              </div>
+            </CommentBox>
             {comments.map((comment, ind) => (
               <CommentEntry key={ind} comment={comment} />
             ))}
