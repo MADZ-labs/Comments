c742f90e550034996077aec7ce119c5b5377598c
Michael Chan <mikey.chan1@gmail.com>
2018-07-29 00:50:07 -0700

    Incorporate Jest testing setup

---
+++ __tests__/components/app.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import App from '../../client/src/components/App'
+
+// describe("Index Component", () => {
+//   it("should render without throwing an error", () => {
+    
+//   });
+// });
