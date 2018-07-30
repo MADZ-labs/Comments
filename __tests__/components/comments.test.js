99dc30fc75cb7760d473978963c018f320fb5317
Michael Chan <mikey.chan1@gmail.com>
2018-07-30 13:39:52 -0700

    fix eslint errors

--- __tests__/components/comments.test.js
+++ __tests__/components/comments.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import Comments from '../../client/src/components/Comments'
 import sinon from 'sinon';
+import Comments from '../../client/src/components/Comments'
 
 describe('<Comments />', () => {
   let wrapper;
-  beforeEach(() => { wrapper = mount(<Comments/>); });
+  beforeEach(() => { wrapper = mount(<Comments />); });
 
   it('should render without throwing an error', () => {
     expect(wrapper).toHaveLength(1);
@@ -21,7 +21,7 @@ describe('<Comments />', () => {
 
   it('should call componentDidMount', () => {
     sinon.spy(Comments.prototype, 'componentDidMount');
-    const wrapper = mount(<Comments/>)
+    const wrapper = mount(<Comments />);
     expect(Comments.prototype.componentDidMount.calledOnce).toEqual(true);
   });
 });
