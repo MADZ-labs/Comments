2a5d4603cf2575748df2bd05de916f139c18527b
Michael Chan <mikey.chan1@gmail.com>
2018-07-30 11:45:42 -0700

    Change test language to be more clear

--- __tests__/server/server.test.js
+++ __tests__/server/server.test.js
@@ -2,15 +2,15 @@ const request = require('supertest');
 const app = require('../../server/app');
 
 describe('Server Test', () => {
-  it('should response with the GET method', () => {
+  it('should respond with a status code of 200 during a GET method', () => {
     request(app).get('/').expect(200);
   });
 
-  it('should respond to the comments GET', () => {
+  it('should respond with a status code of 200 for the GET request to comments ', () => {
     request(app).get('/:projectName/:projectID/section/comments').expect(200);
   });
 
-  it('should response with the correct content type', () => {
+  it('should respond with the correct content type', () => {
     request(app).get('/:projectName/:projectID/section/comments').expect('Content-Type', /json/);
-  }); 
+  });
 });
