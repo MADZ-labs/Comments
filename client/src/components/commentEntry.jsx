64c9e68e1eaf3847329426f7ee7dc7aea97d6a65
Michael Chan <mikey.chan1@gmail.com>
2018-09-19 11:10:01 -0700

    code clean up

--- client/src/components/commentEntry.jsx
+++ client/src/components/commentEntry.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const MainComment = styled.div`
